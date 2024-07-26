56d16cacb5715e634ec4f52833a5574d154aa8ca
William Kennedy <wkenney@protonmail.com>
2024-07-25 21:01:10 -0400

    Many updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,9 +34,11 @@ document.querySelector(".openChatBtn").addEventListener("click", openForm);
 document.querySelector(".close").addEventListener("click", closeForm);
 
 function openForm() {
-  document.querySelector(".openChat").style.display = "block";
+  document.querySelector(".openChat").style.display = "flex";
+  document.querySelector(".openChatBtn").style.display = "none";
 }
 
 function closeForm() {
   document.querySelector(".openChat").style.display = "none";
+  document.querySelector(".openChatBtn").style.display = "flex";
 }
--- features/chatbot/chatbot.js
+++ features/chatbot/chatbot.js
@@ -7,57 +7,17 @@
  * https://ai.google.dev/gemini-api/docs/get-started/node
  */
 
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-import chalk from 'chalk';
-import ora from 'ora';
-import prompt from 'prompt-sync';
+/*Gemini Object Creation */
 
-const userInput = promptSync(chalk.green('You: '));
+import { ChatVertexAI } from "@langchain/google-vertexai";
 
-while (true) {
-    const userInput = promptSync(chalk.green('You: '));
-    if (userInput.toLowerCase() === 'exit') {
-        console.log(chalk.yellow('Goodbye!'));
-        process.exit(0);
-    }
-    const result = await chat.sendMessage(userInput);
-    const response = result.response;
-    console.log(chalk.blue('AI:'), response.text());
-}
+import { HumanMessage } from "@langchain/core/messages";
 
+const model = new ChatVertexAI({
+  model: "gemini-1.5-flash",
+  temperature: 0
+});
 
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
-  
-  const apiKey = process.env.GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run() {
-    const chatSession = model.startChat({
-      generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-    console.log(result.response.text());
-  }
-  
-  run();
+const result = await model.invoke([new HumanMessage({ content: "Hi! I'm Bob" })]);
+
+console.log(result["content"])
